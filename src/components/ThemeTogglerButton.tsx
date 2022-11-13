d7f9beb0e38a201bed8b8afefeb8597633e0a929
Radja Fajrul Ghufron <radjaoke18@gmail.com>
2022-11-13 11:15:09 +0700

    fix: 🐛 react icons import error
    fix react-icons import error

--- src/components/ThemeTogglerButton.tsx
+++ src/components/ThemeTogglerButton.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { IoSunny, IoMoon } from "react-icons/io5";
+import { IoSunny, IoMoon } from "react-icons/io5/index.js";
 
 const themes = ["light", "dark"];
 
