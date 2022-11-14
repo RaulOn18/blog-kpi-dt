d5c73686331e0fa2c3c22b4a3cee0d7f2004b38a
Radja Fajrul Ghufron <radjaoke18@gmail.com>
2022-11-14 22:35:16 +0700

    refactor: 💡 change logo

---
+++ src/components/icons/helvetica.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Helvetica() {
+  return (
+    <svg
+      className="inline-block transition-transform group-hover:rotate-[20deg]"
+      width="25"
+      height="25"
+      viewBox="0 0 89 73"
+      fill="currentcolor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M3.8147e-06 0H4V68H28.2305L76.5 1.03388H80.2758L30.2699 72.0109L30.2441 71.9927L30.2463 72H4H3.8147e-06L0 68L3.8147e-06 0Z"
+        fill="currentcolor"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M26 50V4V0H30H52L50 4L30 4L30 28.5683L46 4L50 4L30 34.8023L30 45L26 50Z"
+        fill="currentcolor"
+      />
+      <path d="M45.5 59.5L63 35.5L89 72H54L45.5 59.5Z" fill="currentcolor" />
+    </svg>
+  );
+}
