5d8fc9a937e113dd4af60770eeef0d086fd8249b
Radja Fajrul Ghufron <radjaoke18@gmail.com>
2022-11-16 00:08:12 +0700

    fix: 🐛 fix warning svg icon

--- src/components/icons/helvetica.tsx
+++ src/components/icons/helvetica.tsx
@@ -11,14 +11,14 @@ export default function Helvetica() {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M3.8147e-06 0H4V68H28.2305L76.5 1.03388H80.2758L30.2699 72.0109L30.2441 71.9927L30.2463 72H4H3.8147e-06L0 68L3.8147e-06 0Z"
         fill="currentcolor"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M26 50V4V0H30H52L50 4L30 4L30 28.5683L46 4L50 4L30 34.8023L30 45L26 50Z"
         fill="currentcolor"
       />
